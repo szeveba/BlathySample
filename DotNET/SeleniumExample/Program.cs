acde6ead925219075004919a43c9c34df20de089
Szegedi Barnabás <szeveba@gmail.com>
2024-04-22 09:54:07 +0200

    selenium sample added

---
+++ DotNET/SeleniumExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace SeleniumExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new EdgeDriver(new EdgeOptions()
+            {
+                ImplicitWaitTimeout = TimeSpan.FromSeconds(5),
+                LeaveBrowserRunning = true
+            });
+            driver.Url = "https://google.com";
+            driver.FindElement(By.Id("W0wltc")).Click();
+            driver.FindElements(By.Name("btnI")).Last().Click();
+        }
+    }
+}
